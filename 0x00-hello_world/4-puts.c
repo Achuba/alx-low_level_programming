ef89eb4aede6d0d4a45cc9bfda05af52b425f9cb
Achuba <achubadaniel45@gmail.com>
2023-07-13 18:53:29 -0700

    Added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdioi>
+/**
+ *main - This is the entry
+ *
+ *return:  Always 0 (sucess)
+*/
+int main(void)
+{	
+	/** This is a print function */
+	puts("\"Programming is like building a multilingual puzzle,\n");
+	return (0);
+}
